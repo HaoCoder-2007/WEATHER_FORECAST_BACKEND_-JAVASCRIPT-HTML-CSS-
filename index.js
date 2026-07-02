aedb43bc2f03cc396aad66bbc65ecb3cf270969f
HaoCoder-2007 <trangiahao0703@gmail.com>
2026-07-02 11:19:38 +0700

    Update

--- index.js
+++ index.js
@@ -77,16 +77,15 @@ async function sendWeatherUpdate(targetChatId = null) {
 
     if (weather) {
         const tempIcon = weather.temp >= 30 ? '🔥' : (weather.temp < 20 ? '❄️' : '☀️');
-        const uvIcon = weather.uvi >= 8 ? '🔴' : (weather.uvi >= 3 ? '🟠' : '🟢');
-
         const message = `
 ${tempIcon} *Cập nhật thời tiết TP.HCM* ${tempIcon}
 
 🌡️ Nhiệt độ: *${Math.round(weather.temp)}°C*
-🤔 Cảm giác như: *${Math.round(weather.feels_like)}°C*
-💧 Độ ẩm: *${weather.humidity}%*
-📝 Trạng thái: *${weather.description}*
-${uvIcon} Chỉ số UV: *${weather.uvi}*
+-Cảm giác như: *${Math.round(weather.feels_like)}°C*
+-Độ ẩm: *${weather.humidity}%*
+-Trạng thái: *${weather.description}*
+
+[${new Date().toLocaleString('vi-VN')}]
         `;
 
         try {
